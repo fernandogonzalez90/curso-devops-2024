cd7df9905b5e15e3273c892637fca1df6610abf9
Andres <asruiz.sis@gmail.com>
2024-03-14 08:56:00 -0300

    Comandos ejemplo AWS EC2

--- labs/aws/comandos.sh
+++ labs/aws/comandos.sh
@@ -8,3 +8,9 @@ aws configure
 aws iam list-users
 aws s3 ls
 aws sts get-caller-identity
+
+# Ejemplo con EC2
+chmod 400 "devops-course.pem"
+ssh -i "devops-course.pem" ec2-user@ec2-54-90-93-131.compute-1.amazonaws.com
+
+sudo systemctl status httpd
