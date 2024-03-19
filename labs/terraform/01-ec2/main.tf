f545d2085ec56701e72910d28ad772db747814d4
Andres <asruiz.sis@gmail.com>
2024-03-19 09:41:34 -0300

    Agrego codigo para crear un bucket

--- labs/terraform/01-ec2/main.tf
+++ labs/terraform/01-ec2/main.tf
@@ -1,19 +1,28 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "5.41.0"
     }
   }
 }
 
 provider "aws" {
-  region     = "us-east-1"
+  region = "us-east-1"
   # profile = "default"
 }
 
 resource "aws_instance" "myec2" {
-  ami                     = "ami-0d7a109bf30624c99"
-  instance_type           = "t2.micro"
-  key_name = "devops-course"
+  ami           = "ami-0d7a109bf30624c99"
+  instance_type = "t2.micro"
+  key_name      = "devops-course"
+
+  tags = {
+    Name = "ec2-terraform"
+  }
+
+}
+
+resource "aws_s3_bucket" "name" {
+  bucket = "devops-course-2024-c3-2"
 }
