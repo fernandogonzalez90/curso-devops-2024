6c6761a13cc2dc1c51cc23dc182c0b5a7d87d370
Andres <asruiz.sis@gmail.com>
2024-03-26 08:57:58 -0300

    Ejemplo EC2 mas SG

---
+++ labs/terraform/02-ec2-and-sg/main.tf
@@ -0,0 +1,45 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.42.0"
+    }
+  }
+  required_version = "1.7.5"
+}
+
+provider "aws" {
+  region = "us-east-1"
+  # profile = "default"
+}
+
+resource "aws_security_group" "my_sg" {
+  name = "my-test-sg"
+
+  ingress {
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "my_ec2" {
+  ami           = "ami-0d7a109bf30624c99"
+  instance_type = "t2.micro"
+  key_name      = "devops-course"
+  vpc_security_group_ids = [ aws_security_group.my_sg.id ]
+
+  tags = {
+    Name = "ec2-terraform"
+  }
+
+}
