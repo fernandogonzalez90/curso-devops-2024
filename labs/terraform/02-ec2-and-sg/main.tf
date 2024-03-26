42d2eed5c92c2aef132437611524e282153242ab
Andres <asruiz.sis@gmail.com>
2024-03-26 09:24:19 -0300

    Ejemplo outputs

--- labs/terraform/02-ec2-and-sg/main.tf
+++ labs/terraform/02-ec2-and-sg/main.tf
@@ -15,14 +15,13 @@ provider "aws" {
 
 resource "aws_security_group" "my_sg" {
   name = "my-test-sg"
-
+  
   ingress {
     from_port = 80
     to_port   = 80
     protocol  = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-  }
-
+  }    
 
   egress {
     from_port   = 0
@@ -37,7 +36,7 @@ resource "aws_instance" "my_ec2" {
   instance_type = "t2.micro"
   key_name      = "devops-course"
   vpc_security_group_ids = [ aws_security_group.my_sg.id ]
-
+  
   tags = {
     Name = "ec2-terraform"
   }
