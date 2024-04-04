bb8d4f7510af91a64da346c2c3eb7b65320523c0
Andres <asruiz.sis@gmail.com>
2024-04-04 08:50:33 -0300

    ejemplo con count

---
+++ labs/terraform/04-count/main.tf
@@ -0,0 +1,25 @@
+# resource "aws_instance" "myec2-1" {
+#   ami           = "ami-08a52ddb321b32a8c"
+#   instance_type = "t2.micro"
+# }
+
+# resource "aws_instance" "myec2-2" {
+#   ami           = "ami-08a52ddb321b32a8c"
+#   instance_type = "t2.micro"
+# }
+
+# resource "aws_instance" "myec2-3" {
+#   ami           = "ami-08a52ddb321b32a8c"
+#   instance_type = "t2.micro"
+# }
+
+resource "aws_instance" "ec2" {
+  count         = 3
+  ami           = "ami-08a52ddb321b32a8c"
+  instance_type = "t2.micro"
+}
+
+resource "aws_iam_user" "user" {
+  count = 3
+  name  = "user-${count.index}"
+}
