0213c38d9ea438aaea0e7c1a17b9659785bfaaf1
Andres <asruiz.sis@gmail.com>
2024-04-04 09:59:21 -0300

    Ejemplo con modulos hecho en clase

--- labs/terraform/07-modulos/projects/projectA/ec2.tf
+++ labs/terraform/07-modulos/projects/projectA/ec2.tf
@@ -1,5 +1,11 @@
 module "my_ec2" {
-    source = "../../modules/ec2"
-    instance_type = "t3.micro"
+  source        = "../../modules/ec2"
+  instance_type = "t3.micro"
+  #security_groups = [module.security-group.id] Esto funciona si el modulo de ec2 soporta este parametro
 }
 
+module "security-group" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.1.2"
+  name    = "my_sg"
+}
--- labs/terraform/07-modulos/projects/projectA/provider.tf
+++ labs/terraform/07-modulos/projects/projectA/provider.tf
@@ -1,4 +1,21 @@
+# terraform {
+#   backend "s3" {
+#     bucket = "asruiz-remote-backend"
+#     key    = "9-remote-state/terraform.tfstate"
+#     region = "us-east-1"
+#     dynamodb_table = "terraform-lock"
+#   }
+# }
+
+# terraform {
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "5.12.0"
+#     }
+#   }
+# }
+
 provider "aws" {
   region = "us-east-1"
-  # profile = "default"
 }
